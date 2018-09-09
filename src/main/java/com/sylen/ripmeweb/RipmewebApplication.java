e619c363a2a6587464536453a5e974ae0d48fbd8
sylen1 <l.labaj18@gmail.com>
2018-09-09 17:11:14 +0200

    spring boot init

---
+++ src/main/java/com/sylen/ripmeweb/RipmewebApplication.java
@@ -0,0 +1,12 @@
+package com.sylen.ripmeweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RipmewebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RipmewebApplication.class, args);
+    }
+}
